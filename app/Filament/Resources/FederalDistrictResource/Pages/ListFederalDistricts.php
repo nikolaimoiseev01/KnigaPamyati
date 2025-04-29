94f6157cfb7e55a4cebe8a0d6485d1debc5abfab
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2025-04-29 12:16:18 +0300

    minor

--- app/Filament/Resources/FederalDistrictResource/Pages/ListFederalDistricts.php
+++ app/Filament/Resources/FederalDistrictResource/Pages/ListFederalDistricts.php
@@ -14,7 +14,7 @@ class ListFederalDistricts extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+//            Actions\CreateAction::make(),
         ];
     }
 }
