69a0ba9b2478c94345f2bb644394ec86c5a649ed
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2025-05-12 23:38:53 +0300

    minor

---
+++ resources/views/livewire/pages/portal/thanks-page.blade.php
@@ -0,0 +1,7 @@
+<main class="flex-1">
+    <div style="background-image: url('/fixed/veterans_list_background.jpeg')" class="pb-16">
+        <div class="w-full text-center py-16">
+            <h1 class="text-6xl lg:text-5xl md:!text-4xl">Благодарности</h1>
+        </div>
+    </div>
+</main>>
